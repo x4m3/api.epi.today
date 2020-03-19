5618cfb90121e33c63d29481ba85214dd2278f67
Philippe Loctaux <p@philippeloctaux.com>
2020-03-20 00:53:38 +0100

    documentation for intra_client

--- src/intra_client.rs
+++ src/intra_client.rs
@@ -1,6 +1,21 @@
 use reqwest;
 use std::time::Duration;
 
+/// Returns the result of a request to the intra
+///
+/// # Arguments
+///
+/// * `client` - A reqwest client already built
+/// * `url` - A string containing the url of the request to execute
+///
+/// # Example
+///
+/// ```
+/// use crate::intra_client;
+/// let request_url = format!("https://intra.epitech.eu/?format=json");
+/// let client = intra_client::create_client()?;
+/// let res = client.get(&request_url).send().await?;
+/// ```
 pub async fn make_get_request(
     client: &reqwest::Client,
     url: &str,
@@ -8,6 +23,14 @@ pub async fn make_get_request(
     Ok(client.get(url).send().await?)
 }
 
+/// Returns a client ready to be used for making requests to the intra
+///
+/// # Example
+///
+/// ```
+/// use crate::intra_client;
+/// let client = intra_client::create_client()?;
+/// ```
 pub fn create_client() -> Result<reqwest::Client, reqwest::Error> {
     let timeout = Duration::new(5, 0);
 
