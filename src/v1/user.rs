93facc1a52579e7f20073b95cda22fd157de688f
Philippe Loctaux <p@philippeloctaux.com>
2020-03-20 17:59:54 +0100

    getting json from data, need to store correctly

--- src/v1/user.rs
+++ src/v1/user.rs
@@ -2,21 +2,22 @@ use crate::intra_autologin;
 use crate::intra_client;
 use actix_web::{get, http::StatusCode, web, HttpRequest, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Serialize)]
 struct ReplyInfo {
     msg: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct UserInfo {
     name: String,
     email: String,
     city: String,
-    year: u8,
-    semester: u8,
-    credits: u16,
-    gpa: f64,
+    year: u64,
+    semester: u64,
+    credits: u64,
+    gpa: String,
     log: f64,
 }
 
@@ -49,21 +50,27 @@ async fn info(req: HttpRequest) -> impl Responder {
         }
     };
 
-    // TODO: deserialize res into structure
-    // TODO: serialize into output
-
-    match res.status() {
-        StatusCode::OK => {
-            return HttpResponse::Ok().json(ReplyInfo {
-                msg: String::from("okay"),
-            });
-        }
-        _ => {
-            return HttpResponse::InternalServerError().json(ReplyInfo {
-                msg: String::from("could not get user information"),
-            });
-        }
+    if res.status() != StatusCode::OK {
+        return HttpResponse::InternalServerError().json(ReplyInfo {
+            msg: String::from("could not get user information"),
+        });
     }
+
+    let raw_body = res.text().await.unwrap();
+    let raw_json: Value = serde_json::from_str(&raw_body).unwrap();
+
+    let user = UserInfo {
+        name: String::from(raw_json["title"].as_str().unwrap()),
+        email: String::from(raw_json["internal_email"].as_str().unwrap()),
+        city: String::from(raw_json["groups"][0]["title"].as_str().unwrap()),
+        year: raw_json["studentyear"].as_u64().unwrap(),
+        semester: raw_json["semester"].as_u64().unwrap(),
+        credits: raw_json["credits"].as_u64().unwrap(),
+        gpa: String::from(raw_json["gpa"][0]["gpa"].as_str().unwrap()),
+        log: raw_json["nsstat"]["active"].as_f64().unwrap(),
+    };
+
+    HttpResponse::Ok().json(user)
 }
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
