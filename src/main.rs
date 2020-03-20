6a9e9bc651d7be1f119ed647b262b218f99661dd
Philippe Loctaux <p@philippeloctaux.com>
2020-03-20 17:57:53 +0100

    big cleanup in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,13 @@
-use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder}; // for web server
-use dotenv::dotenv; // for .env file
-use reqwest; // for http client
-use serde::{Deserialize, Serialize}; // for json serial / de-serial
-use std::{
-    env,            // for system envs
-    time::Duration, // for timeout
-};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
 
 #[macro_use]
 extern crate log;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Test {
-    status_code: u16,
-    status_msg: String,
-    autologin: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Autologin {
-    autologin: String,
-}
-
-async fn index_post(item: web::Json<Autologin>) -> impl Responder {
-    println!("model: {:?}", &item); // show data received
-
-    let ret: Test = Test {
-        status_code: 200,
-        status_msg: String::from("okay"),
-        autologin: item.autologin.clone(), // allocate memory and copy
-    };
-    println!("model: {:?}", &ret);
-
-    web::Json(ret)
-}
-
-async fn simple_json() -> Result<(), reqwest::Error> {
-    println!("test");
-
-    let request_url = format!("https://intra.epitech.eu/auth-5095dbdcd778bdf9bfee368f2729c84bd357c1ea/planning/4686/events?format=json&start=2020-03-11&end=2020-03-11");
-
-    let client = intra_client::create_client()?;
-    let res = client.get(&request_url).send().await?;
-
-    println!("http return code: {}", res.status());
-
-    let body = res.text().await?;
-    println!("Body:\n\n{}", body);
-
-    println!("simplejson done");
-    Ok(())
-}
-
-#[get("/test")]
-async fn index_get() -> impl Responder {
-    let mut list: Vec<Test> = Vec::new(); // list
-
-    list.push(Test {
-        status_code: 200,
-        status_msg: String::from("okay"),
-        autologin: String::from("tek"),
-    });
-    list.push(Test {
-        status_code: 404,
-        status_msg: String::from("not found"),
-        autologin: String::from("tek"),
-    });
-    list.push(Test {
-        status_code: 500,
-        status_msg: String::from("server error"),
-        autologin: String::from("tek"),
-    });
-
-    println!("model: {:?}", &list);
-
-    simple_json().await;
-    println!("request done");
-
-    web::Json(list)
-}
+mod intra_autologin;
+mod intra_client;
+mod v1;
 
 #[get("/")]
 async fn root_doc() -> impl Responder {
@@ -88,10 +17,6 @@ async fn root_doc() -> impl Responder {
         // and no need to have the html file at runtime
         .body(include_str!("../doc/doc.html"))
 }
-
-mod intra_autologin;
-mod intra_client;
-mod v1;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -110,7 +35,6 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::new("[HTTP %s] [URL %U]"))
             .service(root_doc)
             .service(web::scope("/v1").configure(v1::init_routes))
-            .service(index_get)
     });
 
     info!(
