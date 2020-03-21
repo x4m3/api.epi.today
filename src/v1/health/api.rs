134615c885626ba414b57aeeef0877d1bd2616a1
Philippe Loctaux <p@philippeloctaux.com>
2020-03-21 01:52:12 +0100

    /health/api returns api version

--- src/v1/health/api.rs
+++ src/v1/health/api.rs
@@ -4,6 +4,6 @@ use actix_web::{get, HttpResponse, Responder};
 #[get("/api")]
 pub async fn api() -> impl Responder {
     HttpResponse::Ok().json(data::Default {
-        msg: String::from("2epi2day4you"),
+        msg: String::from(env!("CARGO_PKG_VERSION")),
     })
 }
