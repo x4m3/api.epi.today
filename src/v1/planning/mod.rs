971cf4320834b89dc365cde2748269cd9e911e89
Philippe Loctaux <p@philippeloctaux.com>
2020-03-22 06:27:07 +0100

    added /planning/event -> unregister

--- src/v1/planning/mod.rs
+++ src/v1/planning/mod.rs
@@ -1,7 +1,9 @@
 use actix_web::web;
 
 mod event_register;
+mod event_unregister;
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(event_register::event_register);
+    cfg.service(event_unregister::event_unregister);
 }
