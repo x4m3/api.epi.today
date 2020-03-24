1a289b0125d2fb10c31bbe130cf757967a4666a3
Philippe Loctaux <p@philippeloctaux.com>
2020-03-24 10:46:10 +0100

    added stats to server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use actix_web_prom::PrometheusMetrics;
 use dotenv::dotenv;
 use std::env;
 
@@ -19,19 +20,23 @@ async fn root_doc() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    // starting logger
+    // Starting logger
     env::set_var("RUST_LOG", "info");
     env_logger::init();
     info!("hello world");
 
-    // reading env file
+    // Reading env file
     dotenv().ok().expect("Failed to read .env file");
     let host = env::var("HOST").expect("Host not set");
     let port = env::var("PORT").expect("Port not set");
 
-    let server = HttpServer::new(|| {
+    // Stats
+    let prometheus = PrometheusMetrics::new("api", Some("/stats"), None);
+
+    let server = HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::new("[HTTP %s] [URL %U]"))
+            .wrap(prometheus.clone())
             .service(root_doc)
             .service(web::scope("/v1").configure(v1::init_routes))
     });
